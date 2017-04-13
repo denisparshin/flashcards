ceae3191dd31955c874e1c87ed25ec576967c75f
denisparshin <denisparshin92@gmail.com>
2017-04-13 21:45:06 +0300

    Third task

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,46 @@
 class CardsController < ApplicationController
+  
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+ 
+    if @card.update(card_params)
+      redirect_to(:action => 'index')
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.review_date = Time.now
+    if @card.save  
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
+  end
+
+  def destroy
+    @card = Card.destroy(params[:id])
+    redirect_to(:action => 'index')
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
+
 end
