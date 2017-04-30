b1719731b5e1e1e61b17815bed34e9f021accbcb
denisparshin <denisjob2@mail.ru>
2017-04-30 15:16:52 +0300

    Update trainer_controller.rb

--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -2,14 +2,9 @@ class TrainerController < ApplicationController
   
   def index
     @card = Card.pending.first
-      if @card 
-        @card
-      else
-        redirect_to :back, notice: "Нет карточек для пересмотра"
-      end
   end
  
- def check_word
+  def check_word
 
     result = CheckCard.call(
     card_id: params[:card_id],
@@ -20,6 +15,6 @@ class TrainerController < ApplicationController
       redirect_to trainer_path, notice: result.notice
     end
   
-end
+  end
   
 end
